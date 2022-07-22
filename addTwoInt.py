4aee02dea78695dc819e1dd6f986e39f707a5229
Avinash <ahbabajee@student.udm.ac.mu>
2022-07-22 06:15:18 +0400

    add function

---
+++ addTwoInt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+def add_num(a,b):
+    sum=a+b
+    return sum;
+
+a = input("entrez la premiere valeur:")
+b = input("entrez la deuxieme valeur" )
+
+print("La somme est:",add_num(a,b))
+
